2c969a85480637d7bb068a3c1c0ac305d80b0e91
Seokju Hong <iamseokjuhong@gmail.com>
2024-09-18 23:06:56 +0900

    1단계 작성 - 라인 생성 테스트

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -6,11 +6,9 @@ import ladder.generator.PointGenerator;
 
 public class Ladder {
 
-    private final PointGenerator pointGenerator;
     private final Lines lines;
 
     public Ladder(PointGenerator pointGenerator, final Size size) {
-        this.pointGenerator = pointGenerator;
         List<Line> lines = new ArrayList<>();
         for (int i = 0; i < size.getHeight(); i++) {
             lines.add(new Line(pointGenerator, size.getWidth()));
--- src/main/java/ladder/generator/PointGenerator.java
+++ src/main/java/ladder/generator/PointGenerator.java
@@ -2,8 +2,8 @@ package ladder.generator;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface PointGenerator {
-
 
     List<Boolean> generate(int width);
 }
