fa6b359a75b0d183d9cfda87424464bde783a903
Seokju Hong <iamseokjuhong@gmail.com>
2024-12-11 23:12:10 +0900

    불필요한 필드 제거

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -2,16 +2,10 @@ package ladder;
 
 public class Position {
 
-    private final int startIndex;
     private int currentIndex;
 
     public Position(int startIndex) {
-        this.startIndex = startIndex;
         this.currentIndex = startIndex;
-    }
-
-    public int getStartIndex() {
-        return startIndex;
     }
 
     public int getCurrentIndex() {
